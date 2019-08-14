a71632e5b480cfc0a151ca010ac954b7e3b47361
rohanm123 <53967386+rohanm123@users.noreply.github.com>
2019-08-14 18:04:45 +0530

    first

---
+++ Binary.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.functional;
+
+import java.util.Scanner;
+
+import com.bridgelabz.utility.Utility;
+
+public class Binary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		Utility utility=new Utility();
+		System.out.println("enter decimal number");
+		int num=sc.nextInt();
+		Utility.binary(num);
+
+	    }
+	}
+
+
